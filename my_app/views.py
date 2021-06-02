6eb3bec03e09be2a53b6c4162a0866b8a7f004de
Ivan Mischenko <ivan.mischenko@instinc.tools>
2021-06-02 11:41:00 +0300

    custom changes

--- my_app/views.py
+++ my_app/views.py
@@ -6,6 +6,6 @@ from dokkutest.celery import print_task
 # Create your views here.
 
 def index(request):
-    # text = print_task.delay()
-    text = 'Celery doesn\'t work:('
+    text = print_task.delay()
+    # text = 'Celery doesn\'t work:('
     return HttpResponse(content=f'Works!: request.path - {request.path}. Celery text: {text}.')
