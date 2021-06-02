3b4ca5b0dcc5dbc858cf15d516ee9308fc020a9f
Ivan Mischenko <ivan.mischenko@instinc.tools>
2021-06-02 11:26:31 +0300

    custom changes

--- dokkutest/celery.py
+++ dokkutest/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dokkutest.settings')
 
-app = Celery('dokkutest')
+app = Celery('dokkutest', broker='amqp://rabbitmq:377314049310c2b920b9629a0ada5960@dokku-rabbitmq-rabbitmq:5672/rabbitmq')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
