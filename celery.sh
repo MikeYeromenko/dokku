5de433037d664feb078bfd7afa4f716f1a59f9fa
Ivan Mischenko <ivan.mischenko@instinc.tools>
2021-06-03 10:00:20 +0300

    Custom commit

--- celery.sh
+++ celery.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo echo 'sdfsdfsdf'
-sudo pwd
-sudo ls -a
-sudo pip freeze
+echo 'sdfsdfsdf'
+pwd
+ls -a
+pip freeze
 sudo python --version
 sudo celery -A dokkutest worker -l INFO
 sudo celery -A dokkutest -b amqp://rabbitmq:377314049310c2b920b9629a0ada5960@dokku-rabbitmq-rabbitmq:5672/rabbitmq worker
