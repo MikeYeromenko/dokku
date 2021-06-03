8b29707adda69da8e19f42f453b7281f18322a9c
Ivan Mischenko <ivan.mischenko@instinc.tools>
2021-06-03 09:34:11 +0300

    Updates nginx conf

--- my_app/views.py
+++ my_app/views.py
@@ -7,8 +7,8 @@ from dokkutest.celery import print_task
 # Create your views here.
 
 def index(request):
-    text = print_task.delay()
+    res = print_task.delay()
     # text = 'Celery doesn\'t work:('
     while not res.ready():
         time.sleep(1)
-    return HttpResponse(content=f'Works!: request.path - {request.path}. Celery text: {text.result}')
+    return HttpResponse(content=f'Works!: request.path - {request.path}. Celery text: {res.result}')
